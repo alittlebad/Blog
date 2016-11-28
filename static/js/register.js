843e11b421994a5b28861de170d199c5184e10e0
alittlebad <1977848085@qq.com>
2016-11-28 23:37:02 +0800

    zhq

--- static/js/register.js
+++ static/js/register.js
@@ -26,22 +26,46 @@ function validateEmail(){
     var mail=document.getElementById('regEmail').value;
     console.log(mail)
     var filter  = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (!filter.test(mail)) {
-        document.getElementById('errorTips').innerHTML = '请输入正确的邮箱格式';
+    if (filter.test(mail)) {
+        document.getElementById('emailError').innerHTML = '';
+    }else{
+        document.getElementById('emailError').innerHTML = '请输入正确的邮箱格式';
     }
 }
 
 // 验证验证码
-function check(){
+function validateCode(){
     var error;
     // 获取用户输入的验证码
     var codeInput = document.getElementById('codeInput').value;
     if(codeInput.toLowerCase() == code.toLowerCase()){
-        console.log('123');
-        return true;
+        document.getElementById('codeError').innerHTML=''
     }else{
         error = '验证码错误，重新输入';
-        document.getElementById('errorTips').innerHTML = error;
-        return false;
+        document.getElementById('codeError').innerHTML = error;
     }
 }
+function isAgree(){
+    var ischeck=document.getElementById('agree');
+    var isEmail=document.getElementById('emailError').innerHTML==='';
+    var isCode=document.getElementById('codeError').innerHTML==='';
+    if (!ischeck.checked){
+        $('#emailSend').attr('disabled',true);
+    }
+    else{
+        $('#emailSend').attr('disabled',false);
+    }
+}
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(isAgree);
